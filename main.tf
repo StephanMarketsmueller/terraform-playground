0ed00b097479b2ec16cd566a2e4c4d4be64b8b60
Stephan Marketsmüller <stephan.marketsmueller@pentlandfirth.com>
2021-11-21 20:54:37 +0100

    changed ami name bc old one not supported

--- main.tf
+++ main.tf
@@ -3,5 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+    ami = "ami-00d5e377dd7fad751"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
